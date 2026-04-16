58df164d4fc27986ee64f7ec91b7f0834eb4d400
ciyumaomao <ciyumaomao@gmail.com>
2026-04-16 06:56:34 -0800

    Update ub.py

--- ub.py
+++ ub.py
@@ -20,9 +20,9 @@ def keep_alive():
     t = Thread(target=run)
     t.start()
 
-api_id = 20900277
-api_hash = "6a3d761f9be590be4259404f34a1f81e"
-session = "1BVtsOIQBu8ROMVWUG0AF2rZU4triGdflYo28zN5YimAjaCVFmNkiIhSemhNc7YSSqzQYxgTwh9vTtfpMH8X-GSWtr0jMJ_A8yYUAReR0QWe5ZG1q8GslCjpIZE-N_FnN-uwQRZhpRh7p5G58qMniwuhs-U8gt-jhoh8sT4guCL-l4TnWfdj8sh055AShQV_dcbPRPXX6lis11HYGKZotjIl6OzZVdnVGNSrmJU2qvZ1Hgrhii6NTw6cnywzXU0b3R9KA0ILT6v-J35dv_xzTSPs4zC3pmuCpxcGyIvozgBzJxAHnzLE-zvtktD6rVnc2XaCssweNsKmHJk5cS2pHTqGQYYFACA8="
+api_id = 32135350
+api_hash = "7c9832d6ef116e3d75ac843dbc1bbbad"
+session = "1BVtsOIQBu0e_nOgzSD9kxe8NDmuteul2liYvgPlPu3HMJl9ux0Izlg5lceEu631PyIZogd9BI13IwEXGbZ9kyMm82cl7C-2zogtj9fx1ZMIHnjXK02nobKVhYJGQWK429ThrzMqgENCOOfMyrTsGimHE6Siuhxw0RikyoN69sX3QshcIXjdVcHKnaeQRgNqjoT40KzVwa1IhJEQUbmfciu7zWN4eLHT6jQY3xwDW9-XveqagucxBEJyYialU5o5MbXREKBRgzRYrYleC9uYl1jmpq_rpsIUi6gH4ftHrpXa_muicmqXtTt0BXWyoL2yth_F0UQm6yXTIT3w2vYGfJDDr-Uv9qmQ="
 
 client = TelegramClient(StringSession(session), api_id, api_hash)
 
